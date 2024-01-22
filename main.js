6369cc23b70980b482bb4d31dfea1f892467adfe
Anastasia Fazius <AnastasiaFazius@gmx.de>
2024-01-22 12:25:23 +0100

    Adds details for each course

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const coursesApiUrl = 'https://private-e05942-courses22.apiary-mock.com/courses';
 
+  const courseDropdownElement = document.getElementById('courseDropdown');
+  const courseInfoElement = document.getElementById('courseInfo');
+  const totalPriceElement = document.getElementById('totalPrice');
+  const nextStartDateElement = document.getElementById('nextStartDate');
+  const followingStartDatesElement = document.getElementById('followingStartDates');
+
   fetchCourses();
 
   function fetchCourses() {
@@ -20,9 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });      
   }
 
-  function displayCourses(courses) {
-    const courseDropdownElement = document.getElementById('courseDropdown');
 
+  function displayCourses(courses) {    
     courseDropdownElement.innerHTML = '';
 
     courses.forEach(course => {
@@ -32,5 +37,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
       courseDropdownElement.appendChild(optionElement);
     })
+
+    // Add event listener to the dropdown
+    courseDropdownElement.addEventListener('change', () => {
+      const selectedCourseSlug = courseDropdownElement.value;
+
+      // Fetch course details based on the selected course slug
+      fetch(`${coursesApiUrl}/${selectedCourseSlug}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(courseDetails => {
+          displayCourseDetails(courseDetails);          
+        })
+        .catch(error => {
+          console.error('Error fetching course details:', error.message);
+        })
+    })
+  }
+
+
+  function displayCourseDetails(courseDetails) {   
+    courseInfoElement.innerHTML = `<p>${courseDetails.description}</p>`;
+
+    // Additional dateils here
+    totalPriceElement.textContent = `${courseDetails.prices[0].currency} ${courseDetails.prices[0].amount} / ${courseDetails.prices[1].currency} ${courseDetails.prices[1].amount}`;
+    nextStartDateElement.textContent = `${courseDetails.start_dates[0]}`;
+    followingStartDatesElement.textContent = `${courseDetails.start_dates[1]}, ${courseDetails.start_dates[2]}`;
   }
 });
